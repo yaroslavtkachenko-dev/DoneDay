0863d9b5c05175df1c42c1a7f0957fd63e94534e
Yaroslav Tkachenko <57288545+yaroslavtkachenko-dev@users.noreply.github.com>
2025-09-29 20:22:19 +0200

    fix

--- DoneDay/ProjectDeletionView 3.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ProjectDeletionView.swift
-//  DoneDay
-//
-//  Created by Yaroslav Tkachenko on 28.09.2025.
-//
-
-// This file has been consolidated into ProjectManagementView.swift
-// to avoid duplicate declarations.
--- DoneDay/ProjectsCoordinatorView.swift
+++ DoneDay/ProjectsCoordinatorView.swift
@@ -1299,4 +1299,3 @@ struct SettingsActionRow: View {
 #Preview {
     ProjectsCoordinatorView()
 }
-//fef
